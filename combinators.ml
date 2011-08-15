8ac1e67ca99d86e8e278861a18473be62afe4d20
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2011-08-15 03:16:42 +0200

    Exchange the definition of ||| and <|>

--- combinators.ml
+++ combinators.ml
@@ -33,35 +33,36 @@ let ( >>:: ) p q =
 
 let ( >>@ ) p q = ( >>? ) ( @ ) p q
 
-let ( ||| ) p q =
+(* Deterministic alternative operator, tries right only if left fails *)
+let ( <|> ) p q =
     fun input ->
         match p input with
         | [] -> q input
         | x  -> x
 
 (* Non-deterministic alternative operator, tries all possibilities *)
-let ( <|> ) p q = fun input -> p input @ q input
+let ( ||| ) p q = fun input -> p input @ q input
 
-let ( <?> ) p info = p ||| fun input -> raise (Parser.Error (info, input))
+let ( <?> ) p info = p <|> fun input -> raise (Parser.Error (info, input))
 
 let rec many p =
     let continue =
         p      >>= fun x  ->
         many p >>= fun xs ->
         return (x :: xs)
-    in continue ||| mzero
+    in continue <|> mzero
 
 let many1 p = p >>:: many p
 
-let opt p = p ||| mzero
+let opt p = p <|> mzero
 
 let drop p = p >> mzero
 
-let drop_opt p = drop p ||| mzero
+let drop_opt p = drop p <|> mzero
 
 let sep_by1 p sep = p >>:: many (drop sep >> p)
 
-let sep_by p sep = sep_by1 p sep ||| mzero
+let sep_by p sep = sep_by1 p sep <|> mzero
 
 let between o p c = drop o >>@ p >>@ drop c
 
@@ -69,28 +70,28 @@ let chainl1 p op =
     let rec rest x =
         (op >>= fun f ->
          p  >>= fun y ->
-         rest (f x y)) ||| return x
+         rest (f x y)) <|> return x
     in p >>= rest
 
 let rec chainr1 p op =
     let rest x =
         (op           >>= fun f ->
          chainr1 p op >>= fun y ->
-         return (f x y)) ||| return x
+         return (f x y)) <|> return x
     in p >>= rest
 
-let chainl p op x = chainl1 p op ||| return x
+let chainl p op x = chainl1 p op <|> return x
 
-let chainr p op x = chainr1 p op ||| return x
+let chainr p op x = chainr1 p op <|> return x
 
 let choice ps = List.fold_right ( <|> ) ps mzero
 
-let choice1 ps = List.fold_right ( ||| ) ps mzero
+let choice1 ps = List.fold_right ( <|> ) ps mzero
 
 (*
  * If `skip_many' were defined as:
  *
- *     let rec skip_many p = p >> skip_many p ||| mzero
+ *     let rec skip_many p = p >> skip_many p <|> mzero
  *
  * It would cause an infinite loop because first `p' is evaluated, then
  * `skip_many p' and finally `>>'. So if `p' fails, `skip_many p' is evaluated
@@ -136,23 +137,23 @@ let lower = pred (fun c -> 'a' <= c && c <= 'z')
 
 let upper = pred (fun c -> 'A' <= c && c <= 'Z')
 
-let letter = lower ||| upper
+let letter = lower <|> upper
 
-let alphanum = letter ||| digit
+let alphanum = letter <|> digit
 
-let word = many1 (alphanum ||| char '_')
+let word = many1 (alphanum <|> char '_')
 
 let nat = many1 digit
 
 let neg = char '-' >>:: nat
 
-let int = neg ||| nat
+let int = neg <|> nat
 
-let space = (char ' ' ||| char '\t') >>:: mzero
+let space = (char ' ' <|> char '\t') >>:: mzero
 
-let eol = (char '\n' >>:: mzero) ||| (char '\r' >>:: (char '\n' >>:: mzero))
+let eol = (char '\n' >>:: mzero) <|> (char '\r' >>:: (char '\n' >>:: mzero))
 
-let sep = space ||| eol
+let sep = space <|> eol
 
 let junk = many sep
 
@@ -166,7 +167,7 @@ let natural = integer_of_token nat
 
 let negative = integer_of_token neg
 
-let integer = negative ||| natural
+let integer = negative <|> natural
 
 let string s =
     let rec string' = function
@@ -185,7 +186,7 @@ let eof =
 let junk_eof = junk >> eof
 
 let arith_op =
-    (char '+' >> return ( + )) |||
-    (char '-' >> return ( - )) |||
-    (char '*' >> return ( * )) |||
+    (char '+' >> return ( + )) <|>
+    (char '-' >> return ( - )) <|>
+    (char '*' >> return ( * )) <|>
     (char '/' >> return ( / ))
